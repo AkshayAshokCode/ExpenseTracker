d4cfe7acce5ea74f2997eedf6afa9eb15765c918
akshay ashok <akshayashokan1054@gmail.com>
2024-01-27 06:18:26 +0530

    add extension, transactionModel

---
+++ ExpenseTracker/Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  Extensions.swift
+//  ExpenseTracker
+//
+//  Created by Akshay Ashok on 27/01/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color{
+    static let background1 = Color("Background")
+    static let icon1 = Color("Icon")
+    static let text1 = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+                            
+}
